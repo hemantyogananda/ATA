07b0f131accf487587cc58fd9b6029ea7993d906
Annie <prashantyogananda@gmail.com>
2022-05-01 11:45:37 +0530

    1st May changes

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World from Phenom!</Text>
+      <Text>Hello World from Undertaker!</Text>
       <StatusBar style="auto" />
     </View>
   );
